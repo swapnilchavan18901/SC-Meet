96bab3c3a857a2f247cd65b205efe85ea4a98f13
Swapnil Chavan <swapnilchavan18901@gmail.com>
2024-07-27 17:31:04 +0530

    Updated UI theme

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,15 +9,9 @@ const NavBar = () => {
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px6 py-4 lg:px-10">
       
       <Link href='/' className="flex items-center gap-1">
-      <Image
-      src="/icons/logo.svg"
-      width={32}
-      height={32}   
-      alt="yoomlogo"
-      className="max-sm:size-10"   
-      />
+
       <p className='text-[26px] font-extrabold text-white max-sm:hidden'>
-      Yoom
+      SC-Meet
       </p>
 
       </Link>
